813e8d753c99d9d7d64610432e11dbf45a8df2a3
Gábor Ferenc Korbács <gabor.korbacs@stud.uni-corvinus.hu>
2021-10-01 10:35:27 +0200

    2

--- UserMaintanance/Form1.cs
+++ UserMaintanance/Form1.cs
@@ -16,5 +16,10 @@ namespace UserMaintanance
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
