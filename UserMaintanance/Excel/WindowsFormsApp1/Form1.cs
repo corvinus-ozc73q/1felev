dcaf9137c8c2a4a7265248aac3776f9c6e76f187
Gábor Ferenc Korbács <gabor.korbacs@stud.uni-corvinus.hu>
2021-10-08 10:19:42 +0200

    Load Data

--- UserMaintanance/Excel/WindowsFormsApp1/Form1.cs
+++ UserMaintanance/Excel/WindowsFormsApp1/Form1.cs
@@ -12,9 +12,23 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> lakasok;
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+            dataGridView1.DataSource = lakasok;
+        }
+        public void LoadData()
+        {
+            lakasok = context.Flat.ToList();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
