d6213392bc9ae6cbb1fafbfa81de50130057cd5f
Gábor Ferenc Korbács <gabor.korbacs@stud.uni-corvinus.hu>
2021-10-08 11:18:37 +0200

    Excel Coordinate

--- UserMaintanance/Excel/WindowsFormsApp1/Form1.cs
+++ UserMaintanance/Excel/WindowsFormsApp1/Form1.cs
@@ -27,6 +27,7 @@ namespace WindowsFormsApp1
             CreateExcel();
             CreateTable();
 
+
         }
         public void LoadData()
         {
@@ -54,13 +55,74 @@ namespace WindowsFormsApp1
             }
 
         }
-        private void CreateTable();
+        private void CreateTable()
         {
+            string[] headers = new string[]
+                 {
+                   "Kód",
+                        "Eladó",
+                         "Oldal",
+                         "Kerület",
+                         "Lift",
+                         "Szobák száma",
+"Alapterület (m2)",
 
+     "Ár (mFt)",
+     "Négyzetméter ár (Ft/m2)"
+
+                 };
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i + 1] = headers[i];
+
+                object[,] values = new object[lakasok.Count, headers.Length];
+
+                int counter = 0;
+                foreach (var lakas in lakasok)
+                {
+
+                    values[counter, 0] = lakas.Code;
+                    values[counter, 1] = lakas.Vendor;
+                    values[counter, 2] = lakas.Side;
+                    values[counter, 3] = lakas.District;
+                    if (lakas.Elevator == true)
+
+                        values[counter, 4] = "Van";
+                    else
+                        values[counter, 4] = "Nincs";
+
+                        values[counter, 4] = lakas.Elevator;
+               values[counter, 5] = lakas.NumberOfRooms;
+                        values[counter, 6] = lakas.FloorArea;
+                        values[counter, 7] = lakas.Price;
+                        values[counter, 8] = lakas.Code;
+                        counter++;
+                    
+                    var range = xlSheet.get_Range(
+                        GetCell(1, 1),
+                        GetCell(values.GetLength(0), values.GetLength(1)));
+                    range.Value2 = values;
+                }
+
+                string GetCell(int x, int y)
+                {
+                    string ExcelCoordinate = "";
+                    int dividend = y;
+                    int modulo;
+
+                    while (dividend > 0)
+                    {
+                        modulo = (dividend - 1) % 26;
+                        ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                        dividend = (int)((dividend - modulo) / 26);
+                    }
+                    ExcelCoordinate += x.ToString();
+
+                    return ExcelCoordinate;
+                }
+
+
+            }
         }
-        
-        
-
-        
     }
 }
