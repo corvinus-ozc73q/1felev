ab56cbbd7e741aaf3d909bfe68a261d8889e4e6a
Gábor Ferenc Korbács <gabor.korbacs@stud.uni-corvinus.hu>
2021-10-29 20:51:20 +0200

    Diagram

--- WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/Form1.cs
@@ -72,6 +72,22 @@ foreach (XmlElement element in xml.DocumentElement)
         var value = decimal.Parse(childElement.InnerText);
     if (unit != 0)
         rate.Value = value / unit;
+
+            chartRateData.DataSource = Rates;
+
+var series = chartRateData.Series[0];
+        series.ChartType = SeriesChartType.Line;
+series.XValueMember = "Date";
+series.YValueMembers = "Value";
+series.BorderWidth = 2;
+
+var legend = chartRateData.Legends[0];
+        legend.Enabled = false;
+
+var chartArea = chartRateData.ChartAreas[0];
+        chartArea.AxisX.MajorGrid.Enabled = false;
+chartArea.AxisY.MajorGrid.Enabled = false;
+chartArea.AxisY.IsStartedFromZero = false;
 }
 }
 }
