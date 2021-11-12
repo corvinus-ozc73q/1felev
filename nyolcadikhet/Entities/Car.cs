c7f01e46a806bb585b2525454ff7d9eead819f97
Gábor Ferenc Korbács <gabor.korbacs@stud.uni-corvinus.hu>
2021-11-12 14:42:35 +0100

    Auto

---
+++ nyolcadikhet/Entities/Car.cs
@@ -0,0 +1,20 @@
+﻿using nyolcadikhet.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nyolcadikhet.Entities
+{
+    class Car
+    {
+        public class Car : Toy
+        {
+            protected override void DrawImage(Graphics g)
+            {
+                Image imageFile = Image.FromFile("Images/car.png");
+                g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+            }
+        }
+    }
