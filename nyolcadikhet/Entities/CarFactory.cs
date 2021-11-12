5370259166f0158143ec0893cf4eb65ec250190a
Gábor Ferenc Korbács <gabor.korbacs@stud.uni-corvinus.hu>
2021-11-12 14:48:33 +0100

    Autó gyár

--- nyolcadikhet/Entities/CarFactory.cs
+++ nyolcadikhet/Entities/CarFactory.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using nyolcadikhet.Abstractions
+﻿using nyolcadikhet.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,12 +7,11 @@ using System.Threading.Tasks;
 
 namespace nyolcadikhet.Entities
 {
-     public class BallFactory : ToyFactory
+    public class CarFactory : IToyFactory
     {
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
     }
 }
-
