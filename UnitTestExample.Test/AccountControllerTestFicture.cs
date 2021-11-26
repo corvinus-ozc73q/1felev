c2bdd1fbfce89b7cb4b654916031d313cd6e9a77
Gábor Ferenc Korbács <gabor.korbacs@stud.uni-corvinus.hu>
2021-11-26 22:18:00 +0100

    Alosztaly 2

--- UnitTestExample.Test/AccountControllerTestFicture.cs
+++ UnitTestExample.Test/AccountControllerTestFicture.cs
@@ -46,4 +46,32 @@ var actualResult = accountController.Register(email, password);
 Assert.AreEqual(password, actualResult.Password);
 Assert.AreNotEqual(Guid.Empty, actualResult.ID);
 accountServiceMock.Verify(m => m.CreateAccount(actualResult), Times.Once);
+
+        [
+    Test,
+    TestCase("irf@uni-corvinus.hu", "Abcd1234")
+]
+    public void TestRegisterApplicationException(string newEmail, string newPassword)
+    {
+        // Arrange
+        var accountServiceMock = new Mock<IAccountManager>(MockBehavior.Strict);
+        accountServiceMock
+            .Setup(m => m.CreateAccount(It.IsAny<Account>()))
+            .Throws<ApplicationException>();
+        var accountController = new AccountController();
+        accountController.AccountManager = accountServiceMock.Object;
+
+        // Act
+        try
+        {
+            var actualResult = accountController.Register(newEmail, newPassword);
+            Assert.Fail();
+        }
+        catch (Exception ex)
+        {
+            Assert.IsInstanceOf<ApplicationException>(ex);
+        }
+
+        // Assert
+    }
 }
