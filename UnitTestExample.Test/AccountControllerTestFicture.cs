5566ef14cfeb5789e74eafaa4c7467553bd6acb9
Gábor Ferenc Korbács <gabor.korbacs@stud.uni-corvinus.hu>
2021-11-26 21:55:54 +0100

    EmailValidateTestVoid

---
+++ UnitTestExample.Test/AccountControllerTestFicture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFicture
+    {
+      //  [Test]
+        public void TestValidateEmail(string email, bool expectedresult)
+        { 
+        }
+    }
+}
---
+++ UnitTestExample.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestExample.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("UnitTestExample.Test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("674bbbe4-f6ed-4e54-8846-d4e8bdec11e3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
