db955bf9a9a8e01addbf30f68f61591076f938e4
Gábor Ferenc Korbács <gabor.korbacs@stud.uni-corvinus.hu>
2021-12-03 13:41:05 +0100

    Osztalyok

---
+++ tizenegyedikhet/Entities/DeathProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tizenegyedikhet.Entities
+{
+    public class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+
+        public double DProbability { get; set; }
+    }
+}
