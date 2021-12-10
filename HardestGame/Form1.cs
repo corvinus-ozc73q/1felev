aea2eca9e2e1cc0a4bca9715350458fa2e494708
Gábor Ferenc Korbács <gabor.korbacs@stud.uni-corvinus.hu>
2021-12-10 17:43:51 +0100

    Verseny felépítése

--- HardestGame/Form1.cs
+++ HardestGame/Form1.cs
@@ -79,5 +79,14 @@ namespace HardestGame
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gc.ResetCurrentLevel();
+            gc.AddPlayer(winnerBrain.Clone());
+            gc.AddPlayer();
+            ga.Focus();
+            gc.Start(true);
+        }
     }
 }
